a8f20314370f769b113be399636b7af582eff00e
Darsh879 <79584169+Darsh879@users.noreply.github.com>
2021-12-13 11:19:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+song="";
+rightWristY = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('poseNet is initialized');
+} 
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log('results');
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristY = " + rightWristY);
+    }
+    
+}
